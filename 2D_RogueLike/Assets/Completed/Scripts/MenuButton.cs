b946eed0cde91eb114b6a5cb76e32661f1f1a3a0
Gerigon <Rutger-100@hotmail.com>
2016-05-17 12:38:43 +0200

    Menu Buttons
    Added OnMouseButton Down effects and changed the buttons :yellow_heart:

---
+++ 2D_RogueLike/Assets/Completed/Scripts/MenuButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler
+{
+    public Text theText;
+
+    public void OnPointerDown(PointerEventData evd)
+    {
+        theText.color = new Color(1f, 0.4f, 0f); //Or however you do your color
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        theText.color = new Color(1f, 0.4f, 0f); //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = new Color(1f,1f,1f); //Or however you do your color
+    }
+}
