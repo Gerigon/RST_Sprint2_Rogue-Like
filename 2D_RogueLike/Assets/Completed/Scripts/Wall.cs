5380359d453b9620f250f843b53432c839a9177f
s7ubborn <s7ubborn@gmail.com>
2016-05-20 16:05:38 +0200

    Pile of stones after you break a block, changed music
    Adjusted the volumes of both of the Audio Listeners so the sound effects
    are louder and have more priority than the music. Added 3 new sprites
    for stones.

--- 2D_RogueLike/Assets/Completed/Scripts/Wall.cs
+++ 2D_RogueLike/Assets/Completed/Scripts/Wall.cs
@@ -8,16 +8,19 @@ namespace Completed
 		public AudioClip chopSound1;				//1 of 2 audio clips that play when the wall is attacked by the player.
 		public AudioClip chopSound2;				//2 of 2 audio clips that play when the wall is attacked by the player.
 		public Sprite dmgSprite;					//Alternate sprite to display after Wall has been attacked by player.
+        public Sprite stoneSprite;                  //Alternate sprite to be shown on this position after Wall has been destroyed.
 		public int hp = 3;							//hit points for the wall.
 		
 		
 		private SpriteRenderer spriteRenderer;		//Store a component reference to the attached SpriteRenderer.
+        private BoxCollider2D box2d;
 		
 		
 		void Awake ()
 		{
 			//Get a component reference to the SpriteRenderer.
 			spriteRenderer = GetComponent<SpriteRenderer> ();
+            box2d = GetComponent<BoxCollider2D>();
 		}
 		
 		
@@ -32,11 +35,16 @@ namespace Completed
 			
 			//Subtract loss from hit point total.
 			hp -= loss;
-			
-			//If hit points are less than or equal to zero:
-			if(hp <= 0)
-				//Disable the gameObject.
-				gameObject.SetActive (false);
+            Debug.Log(hp);
+
+            //If hit points are less than or equal to zero:
+            if (hp <= 0)
+            {
+                //Change the sprite of the gameObject.
+                spriteRenderer.sprite = stoneSprite;
+                //Disable the gameObject.
+                box2d.enabled = false;
+            }
 		}
 	}
 }
